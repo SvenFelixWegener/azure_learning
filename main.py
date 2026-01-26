758e4d2b9e418d21c47bd9c7a80891ca871f7a16
svenwegener <sven@sven-wegener.de>
2026-01-26 15:37:21 +0100

    Make Python a Webapp with FastAPI

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import os
-from flask import Flask, jsonify
-
-app = Flask(__name__)
+from fastapi import FastAPI
+app = FastAPI()
 
 @app.get("/")
 def health():
-    return jsonify(status="ok")
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
-    app.run(host="0.0.0.0", port=port)
+    return {"status": "ok"}
